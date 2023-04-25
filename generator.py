180e2835a8615f1b7a5e9985956a50eeaa0b365a
ango0817 <andrei.golosov@netcracker.com>
2023-04-25 13:26:10 +0530

    Refactoring

--- generator.py
+++ generator.py
@@ -19,19 +19,14 @@ worksheet.append(['Address', 'Password', 'Seed Phrase', 'Private Key'])
 password = ""
 
 for i in range(10):
-    mnemonic_object = Mnemonic("english")
-    mnemonic_phrase = mnemonic_object.generate(128)
-    test = BIP44HDWallet(symbol="ETH")
-    wallet = test.from_mnemonic(mnemonic_phrase)
+    mnemonic_phrase = Mnemonic("english").generate(128)
+    hd_wallet = BIP44HDWallet(symbol="ETH")
+    wallet = hd_wallet.from_mnemonic(mnemonic_phrase)
     worksheet.append([wallet.address(), password, mnemonic_phrase, wallet.private_key()])
-
 
 alignment = Alignment(horizontal='center', vertical='center')
 for row in worksheet.rows:
     for cell in row:
         cell.alignment = alignment
 
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-
-# Save the workbook to a file
-workbook.save(f'generated_wallets_{timestamp}.xlsx')
+workbook.save(f'generated_wallets_{datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}.xlsx')
